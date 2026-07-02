e2531f2d9b446c9520834795a969bb2e769fb971
piyush saroj <piyushsaroj222006@gmail.com>
2026-07-03 01:46:42 +0530

    local

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,21 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? "http://localhost:3000/api"
+//       : "/api",
+//   withCredentials: true,
+// });
+
 import axios from "axios";
+
+const hostname = window.location.hostname;
 
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
+      ? `http://${hostname}:3000/api`
       : "/api",
   withCredentials: true,
 });
