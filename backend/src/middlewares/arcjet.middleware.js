848f37ab957788e7ffbcb34ab4e2c853bdaba7a0
piyush saroj <piyushsaroj222006@gmail.com>
2026-07-25 14:55:24 +0530

    7

--- backend/src/middlewares/arcjet.middleware.js
+++ backend/src/middlewares/arcjet.middleware.js
@@ -7,35 +7,29 @@ export const arcjetMiddleware = async (req, res, next) => {
 
     if (decision.isDenied()) {
       if (decision.reason.isRateLimit()) {
-        return res.status(429).json({
-          message: "Rate limit exceeded. Please try again later.",
-        });
+        res
+          .status(429)
+          .json({ message: "Rate limit exceeded. Please try again later." });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ message: "Access denied: Bot detected" });
+      } else {
+        return res
+          .status(403)
+          .json({ message: "Access denied: Suspicious activity detected" });
       }
-
-      if (decision.reason.isBot()) {
-        return res.status(403).json({
-          message: "Access denied: Bot detected",
-        });
-      }
-
-      return res.status(403).json({
-        message: "Access denied: Suspicious activity detected",
-      });
     }
 
+    // check for spoofed bots
     if (decision.results.some(isSpoofedBot)) {
       return res.status(403).json({
         error: "Spoofed bot detected",
         message: "Malicious bot activity detected.",
       });
     }
-
-    return next();
+    next();
   } catch (error) {
     console.error("Error in Arcjet middleware:", error);
-
-    return res.status(500).json({
-      message: "Internal Server Error",
-    });
+    res.status(500).json({ message: "Internal Server Error" });
+    next();
   }
 };
