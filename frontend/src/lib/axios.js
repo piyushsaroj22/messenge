d63f39dcd710584c21f130a3249f63cb606c3538
piyush saroj <piyushsaroj222006@gmail.com>
2026-07-25 16:15:25 +0530

    final push hai ye

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,27 +1,3 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL:
-//     import.meta.env.MODE === "development"
-//       ? "http://localhost:3000/api"
-//       : "/api",
-//   withCredentials: true,
-// });
-
-//=====================================================================================================================================
-
-// import axios from "axios";
-
-// const hostname = window.location.hostname;
-
-// export const axiosInstance = axios.create({
-//   baseURL:
-//     import.meta.env.MODE === "development"
-//       ? `http://${hostname}:3000/api`
-//       : "/api",
-//   withCredentials: true,
-// });
-
 import axios from "axios";
 
 // const hostname = window.location.hostname;
@@ -34,8 +10,3 @@ export const axiosInstance = axios.create({
 });
 
 // import.meta.env.VITE_SERVER_URL ||
-
-// export const axiosInstance = axios.create({
-//   baseURL: `http://${hostname}:3000/api`,
-//   withCredentials: true,
-// });
