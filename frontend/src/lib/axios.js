6b4fcdba60c093ac6aeb1a9f34722b48642b8007
piyush saroj <piyushsaroj222006@gmail.com>
2026-07-25 13:10:07 +0530

    con

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -8,14 +8,32 @@
 //   withCredentials: true,
 // });
 
+//=====================================================================================================================================
+
+// import axios from "axios";
+
+// const hostname = window.location.hostname;
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? `http://${hostname}:3000/api`
+//       : "/api",
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
 const hostname = window.location.hostname;
 
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || `http://${hostname}:3000`;
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? `http://${hostname}:3000/api`
-      : "/api",
+  baseURL: `${SERVER_URL}/api`,
   withCredentials: true,
 });
+
+// export const axiosInstance = axios.create({
+//   baseURL: `http://${hostname}:3000/api`,
+//   withCredentials: true,
+// });
