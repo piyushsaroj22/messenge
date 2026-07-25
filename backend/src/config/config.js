40eb1ceccf4d16119210ce334342a1c29f26fffa
piyush saroj <piyushsaroj222006@gmail.com>
2026-07-25 13:59:28 +0530

    2

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,72 +1,72 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-if (!process.env.MONGO_URI) {
-  throw new Error("MONGO_URI is not defined in the environment variables");
-}
+// if (!process.env.MONGO_URI) {
+//   throw new Error("MONGO_URI is not defined in the environment variables");
+// }
 
-if (!process.env.PORT) {
-  throw new Error("PORT is not defined in the environment variables");
-}
+// if (!process.env.PORT) {
+//   throw new Error("PORT is not defined in the environment variables");
+// }
 
-if (!process.env.JWT_SECRET) {
-  throw new Error("JWT_SECRET is not defined in the environment variables");
-}
+// if (!process.env.JWT_SECRET) {
+//   throw new Error("JWT_SECRET is not defined in the environment variables");
+// }
 
-if (!process.env.NODE_ENV) {
-  throw new Error("NODE_ENV is not defined in the environment variables");
-}
+// if (!process.env.NODE_ENV) {
+//   throw new Error("NODE_ENV is not defined in the environment variables");
+// }
 
-if (!process.env.CLOUDINARY_CLOUD_NAME) {
-  throw new Error(
-    "CLOUDINARY_CLOUD_NAME is not defined in the environment variables",
-  );
-}
+// if (!process.env.CLOUDINARY_CLOUD_NAME) {
+//   throw new Error(
+//     "CLOUDINARY_CLOUD_NAME is not defined in the environment variables",
+//   );
+// }
 
-if (!process.env.CLOUDINARY_API_KEY) {
-  throw new Error(
-    "CLOUDINARY_API_KEY is not defined in the environment variables",
-  );
-}
+// if (!process.env.CLOUDINARY_API_KEY) {
+//   throw new Error(
+//     "CLOUDINARY_API_KEY is not defined in the environment variables",
+//   );
+// }
 
-if (!process.env.CLOUDINARY_API_SECRET) {
-  throw new Error(
-    "CLOUDINARY_API_SECRET is not defined in the environment variables",
-  );
-}
+// if (!process.env.CLOUDINARY_API_SECRET) {
+//   throw new Error(
+//     "CLOUDINARY_API_SECRET is not defined in the environment variables",
+//   );
+// }
 
-if (!process.env.ARCJET_KEY) {
-  throw new Error("ARCJET_KEY is not defined in the environment variables");
-}
+// if (!process.env.ARCJET_KEY) {
+//   throw new Error("ARCJET_KEY is not defined in the environment variables");
+// }
 
-if (!process.env.ARCJET_ENV) {
-  throw new Error("ARCJET_ENV is not defined in the environment variables");
-}
+// if (!process.env.ARCJET_ENV) {
+//   throw new Error("ARCJET_ENV is not defined in the environment variables");
+// }
 
-if (
-  process.env.NODE_ENV !== "development" &&
-  process.env.NODE_ENV !== "production"
-) {
-  throw new Error("NODE_ENV must be either 'development' or 'production'");
-}
+// if (
+//   process.env.NODE_ENV !== "development" &&
+//   process.env.NODE_ENV !== "production"
+// ) {
+//   throw new Error("NODE_ENV must be either 'development' or 'production'");
+// }
 
-if (!process.env.RESEND_API_KEY) {
-  throw new Error("RESEND_API_KEY is not defined in the environment variables");
-}
+// if (!process.env.RESEND_API_KEY) {
+//   throw new Error("RESEND_API_KEY is not defined in the environment variables");
+// }
 
-if (!process.env.EMAIL_FROM) {
-  throw new Error("EMAIL_FROM is not defined in the environment variables");
-}
+// if (!process.env.EMAIL_FROM) {
+//   throw new Error("EMAIL_FROM is not defined in the environment variables");
+// }
 
-if (!process.env.EMAIL_FROM_NAME) {
-  throw new Error(
-    "EMAIL_FROM_NAME is not defined in the environment variables",
-  );
-}
+// if (!process.env.EMAIL_FROM_NAME) {
+//   throw new Error(
+//     "EMAIL_FROM_NAME is not defined in the environment variables",
+//   );
+// }
 
-if (!process.env.CLIENT_URL) {
-  throw new Error("CLIENT_URL is not defined in the environment variables");
-}
+// if (!process.env.CLIENT_URL) {
+//   throw new Error("CLIENT_URL is not defined in the environment variables");
+// }
 
 const config = {
   MONGO_URI: process.env.MONGO_URI,
