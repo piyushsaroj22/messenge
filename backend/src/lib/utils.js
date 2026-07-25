743c7e448783a3152dadd871b1f38a9181957bdf
piyush saroj <piyushsaroj222006@gmail.com>
2026-07-25 21:45:01 +0530

    hogaya

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,6 +6,12 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: config.NODE_ENV === "production",
+    sameSite: config.NODE_ENV === "production" ? "none" : "lax",
+  });
+
   return token;
 };
