cba1b050a7774417c44fa50a72397be21d7f7039
piyush saroj <piyushsaroj222006@gmail.com>
2026-07-25 13:53:45 +0530

    app running

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,5 @@ const PORT = config.PORT;
 connectDB();
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT} (${config.NODE_ENV})`);
 });
